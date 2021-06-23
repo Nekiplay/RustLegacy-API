e46ecacdd71b3272907d720ab4fc26a982044049
Neki_play <neci.03@mail.ru>
2021-06-23 17:48:14 +0500

    Y Position Fixes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -12,8 +13,15 @@ namespace ConsoleApp1
         {
             System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessesByName("rust").FirstOrDefault();
             RustLegacyAPI.API api = new RustLegacyAPI.API(p);
-            //Console.WriteLine("Здововье: " + api.Health);
-            //Console.WriteLine("Еды: " + api.Food);
+            Task.Factory.StartNew(() =>
+            {
+                float temp = api.Y_Position;
+                while (true)
+                {
+                    api.Y_Position = temp;
+                    Thread.Sleep(25);
+                }
+            });
             Console.WriteLine("Y позиция: " + api.Y_Position);
             Console.ReadKey();
         }
--- RustLegacyAPI/Class1.cs
+++ RustLegacyAPI/Class1.cs
@@ -40,8 +40,12 @@ namespace RustLegacyAPI
                 //Console.WriteLine("Базовый адресс: 0x" + clientdll.ToString("X"));
                 IntPtr addres = ReadOffset(memory, clientdll, Offsets.Food);
                 //Console.WriteLine("Адресс здоровья: 0x" + addres.ToString("X"));
-                float health = memory.Read<float>(addres);
-                return health;
+                if (addres != IntPtr.Zero)
+                {
+                    float health = memory.Read<float>(addres);
+                    return health;
+                }
+                else { return 0; }
             }
         }
         public float Y_Position
@@ -53,8 +57,12 @@ namespace RustLegacyAPI
                 IntPtr clientdll = Offsets.PlayerPosition;
                 Console.WriteLine("Базовый адресс: 0x" + clientdll.ToString("X"));
                 IntPtr addres = ReadOffset(memory, clientdll, Offsets.Y_Position);
-                float health = memory.Read<float>(addres);
-                return health;
+                if (addres != IntPtr.Zero)
+                {
+                    float health = memory.Read<float>(addres);
+                    return health;
+                }
+                else { return 0; }
             }
             set
             {
@@ -67,13 +75,20 @@ namespace RustLegacyAPI
         }
         public IntPtr ReadOffset(IMemory memory, IntPtr address, int[] offset)
         {
-            for (int i = 0; i < offset.Count() - 1; i++)
+            try
             {
-                address = memory.Read<IntPtr>(address + offset[i]);
-                Console.WriteLine("0x" + address.ToString("X"));
+                for (int i = 0; i < offset.Count() - 1; i++)
+                {
+                    address = memory.Read<IntPtr>(address + offset[i]);
+                    Console.WriteLine("0x" + address.ToString("X"));
+                }
+                address += offset[offset.Count() - 1];
+                return address;
             }
-            address += offset[offset.Count() - 1];
-            return address;
+           catch
+            {
+                return IntPtr.Zero;
+            }
         }
         public IntPtr ReadOffset(IMemory memory, IntPtr address, int offset)
         {
@@ -88,8 +103,8 @@ namespace RustLegacyAPI
         public static int[] Health = new[] { 0x38, 0x8, 0x50, 0x250, 0x8, 0x24, 0x94 };
         public static int[] Food = new[] { 0x38, 0x8, 0x48, 0x8, 0x554, 0x34, 0x1F4 };
 
-         public static IntPtr PlayerPosition = new IntPtr(0x036C2880);
+         public static IntPtr PlayerPosition = new IntPtr(0x037B8838);
 
-        public static int[] Y_Position = new[] { 0x8, 0x0, 0x10, 0x4, 0x0, 0x0, 0x4C0 };
+        public static int[] Y_Position = new[] { 0x20, 0x10, 0x8, 0x10, 0x4C, 0xA0, 0x238 };
     }
 }
